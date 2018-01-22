b853286e169c6c01ac1daf62d88789c80fbed28e
Clara Andrew <candrewwani@gmail.com>
2018-01-22 15:18:26 -0500

    initial

--- immutable.js
+++ immutable.js
@@ -15,6 +15,14 @@
   Be sure to console.log() both references to ensure that they are pointing to different objects.
 */
 
+const person = {
+  name: 'Clara',
+  age: 25,
+  hometown: 'Detroit'
+}
+
+const newPerson = Object.assign({}, person, {age: 30, name: 'Troy'})
+
 console.log(person);
 console.log(newPerson);
 
@@ -23,6 +31,10 @@ console.log(newPerson);
   Create a separate array of `superVillains`
   Concatenate the two of them together using `.concat()`
 */
+
+const superHeroes = ['batman', 'ironman', 'jessica jones', 'Thor'];
+const superVillains = ['Joker', 'lex luther', 'thanos', 'Dark vader']
+const combinedSupers = [...superHeroes, ...superVillains];
 
 console.log(superHeroes);
 console.log(superVillains);
@@ -35,6 +47,14 @@ console.log(combinedSupers);
   Create a new object `studentAverage` using Object.assign() from the given original object.
   The new object should filter out any scores that are < 34 Array.filter should be used here.
 */
+
+const studentScores = {
+  name: 'Clara',
+  age: 25, 
+  scores: 50
+}
+
+const studentAverage = Object.assign({}, studentScores);
 
 console.log(studentScores);
 console.log(studentAverage);
